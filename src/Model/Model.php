640652983594c3333e10a36e34d59ba78fa60ea9
Emeric <105379288+MrIC1976@users.noreply.github.com>
2024-02-06 11:16:43 +0100

    feat: ajout singleton

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -1,23 +1,34 @@
 <?php
 
-namespace App\Model;
+namespace App\model;
 use PDO;
 
-abstract class Model
-{
+abstract class Model {
     private static $pdo;
+    public $connection;
 
-    private static function setBdd()
-    {
-        self::$pdo = new PDO("mysql:host=localhost;dbname=connecting;charset=utf8", "root", "");
-        self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // Constructeur privé pour empêcher l'instanciation depuis l'extérieur de la classe
+    private function __construct() {
+        // Initialisez votre connexion à la base de données ici
+        $this->connection = new PDO('mysql:host=localhost;dbname=NOM_DE_LA_TABLE', 'root', '');
     }
 
-    protected function getBdd()
-    {
-        if (self::$pdo === null) {
-            self::setBdd();
+    // Méthode statique pour récupérer l'instance unique de la classe Model
+    public static function getBdd() {
+        if (!isset(self::$pdo)) {
+            self::$pdo = new self();
         }
         return self::$pdo;
     }
+
+    // evite l'utilisation de la fonction magique __clone
+    protected function __clone() { }
+
+    // empeche la desserialisation
+    public function __wakeup()
+    {
+        throw new \Exception("impossible de désserialiser");
+    }
+
 }
+
